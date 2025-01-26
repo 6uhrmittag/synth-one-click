59629f6bc9cc58cc28b0925836ffa6df77b3b614
6uhrmittag <marvin@6uhrmittag.de>
2025-01-26 17:05:25 +0100

    AAAAAAAAAAAAAAHHHHHHHHHHHHHHHH

--- bin/synth_one_click_installer.dart
+++ bin/synth_one_click_installer.dart
@@ -36,7 +36,7 @@ String? getSynthRidersFolder() {
       "com.synthriders.installpath_h4259148619",
       path: "SOFTWARE\\Kluge Interactive\\SynthRiders");
 
-  pathv3.join(path, "SynthRidersUC"));
+  pathv3 = path.join(path, "SynthRidersUC");
   if (pathv3 != null && pathv3.type == RegistryValueType.binary) {
     return String.fromCharCodes(pathv3.data as Uint8List);
   }
